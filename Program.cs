80ddc556a32eee0e13d5b92731a6a5dbc66b5330
Gaston Ariel Bordet <gastonb@lagash.com>
2019-05-30 16:49:58 -0300

    Nuevo meto para calcular decimales con division

--- Program.cs
+++ Program.cs
@@ -27,8 +27,8 @@ namespace tareaReloaded
             cociente++;
             return CalcularCocienteRecursiva(dividendo - divisor, divisor, resto, cociente);
         }
-
-        static float DividirDecimalesRecursiva(int dividendo, int divisor, int resto, int cociente = 1, string res = "") 
+        
+        static float DividirDecimalesRecursivaSinDivision(int dividendo, int divisor, int resto, int cociente = 1, string res = "") 
         {
             // * Si el dividendo es 0 el cociente siempre sera 0.
             if (dividendo == 0)
@@ -58,13 +58,45 @@ namespace tareaReloaded
             dividendo = resto * 10;
             resto = dividendo % divisor;
 
-            return DividirDecimalesRecursiva(dividendo, divisor, resto, cociente, res);    
+            return DividirDecimalesRecursivaSinDivision(dividendo, divisor, resto, cociente, res);    
+        }
+
+        static float DividirDecimalesRecursivaConDivision(int dividendo, int divisor, int resto, int cociente = 1, float res = 0) 
+        {
+            // * Si el dividendo es 0 el cociente siempre sera 0.
+            if (dividendo == 0)
+            {
+                return 0;
+            }
+
+            // * Si el resto es 0 el cociente sera entero.
+            if (resto == 0) 
+            {
+                res += CalcularCocienteRecursiva(dividendo, divisor, resto, cociente);
+                //float result = float.Parse(res.ToString());
+                
+                return res / 100;
+            }
+
+            else 
+            {
+                res = res * 10 + CalcularCocienteRecursiva(dividendo, divisor, resto, cociente) * 10;
+            }
+
+            
+            // * Agrega 0 al resto y vuelve a calcular un nuevo resto.
+            dividendo = resto * 10;
+            resto = dividendo % divisor;
+
+            return DividirDecimalesRecursivaConDivision(dividendo, divisor, resto, cociente, res);    
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(DividirDecimalesRecursiva(21, 4, 21%4));
+            Console.WriteLine(DividirDecimalesRecursivaSinDivision(21, 4, 21%4));
+            Console.WriteLine(DividirDecimalesRecursivaConDivision(21, 4, 21%4));
             Console.WriteLine(CalcularCocienteRecursiva(8, 4, 8%4));
+            Console.WriteLine(CalcularCocienteRecursiva(9, 4, 9%4));
             Console.WriteLine(CalcularCocienteIteracion(12, 4, 12%4));
             Console.WriteLine(CalcularCocienteIteracion(13, 4, 13%4));
         }
