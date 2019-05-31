4e5ea749b07d8ff9b038b6bd847be2c1ac4f380e
Gaston Ariel Bordet <gastonb@lagash.com>
2019-05-31 08:33:13 -0300

    Decimales sin string

--- Program.cs
+++ Program.cs
@@ -61,9 +61,40 @@ namespace tareaReloaded
             return DividirDecimalesRecursivaSinDivision(dividendo, divisor, resto, cociente, res);    
         }
 
+        static float DividirDecimalesRecursivaSinDivision2(int dividendo, int divisor, int resto, int cociente = 1, float res = 0) 
+        {
+            // * Si el dividendo es 0 el cociente siempre sera 0.
+            if (dividendo == 0)
+            {
+                return 0;
+            }
+
+            // * Si el resto es 0 el cociente sera entero.
+            if (resto == 0) 
+            {
+                res += CalcularCocienteRecursiva(dividendo, divisor, resto, cociente);
+                if (res < 100) res *= 10; 
+                return res * (float) 0.01;
+            }
+
+            else 
+            {
+                res = res * 10 + CalcularCocienteRecursiva(dividendo, divisor, resto, cociente) * 10;
+            }
+            
+            // * Agrega 0 al resto y vuelve a calcular un nuevo resto.
+            dividendo = resto * 10;
+            resto = dividendo % divisor;
+
+            return DividirDecimalesRecursivaSinDivision2(dividendo, divisor, resto, cociente, res);    
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(DividirDecimalesRecursivaSinDivision(21, 4, 21%4));
+            //Console.WriteLine(DividirDecimalesRecursivaSinDivision(21, 4, 21%4));
+            Console.WriteLine(DividirDecimalesRecursivaSinDivision2(21, 4, 21%4));
+            //Console.WriteLine(DividirDecimalesRecursivaSinDivision(13, 5, 13%5));
+            Console.WriteLine(DividirDecimalesRecursivaSinDivision2(13, 5, 13%5));
             Console.WriteLine(CalcularCocienteRecursiva(8, 4, 8%4));
             Console.WriteLine(CalcularCocienteRecursiva(9, 4, 9%4));
             Console.WriteLine(CalcularCocienteIteracion(12, 4, 12%4));
